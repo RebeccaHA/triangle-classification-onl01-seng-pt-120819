d9ed5c4df7e75cf63aef1367122402704fe49244
calm-block-7153 <rebeccahunt034@gmail.com>
2020-01-23 17:35:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,14 +9,14 @@ class Triangle
    if (@side_1 <=0 || @side_2 <=0 || @side_3 <=0) || (@side_1 + @side_2 <= @side_3 || @side_2 + @side_3 <= @side_1 || @side_1 + @side_3 <= @side_2)
      raise TriangleError
    else  
-     if (@side_1 != @side_2) && (@side_3 != @side_2) && (@side_1 != @side_3)
-      :scalene
-      elsif
+     if @side_1 == @side_2 && @side_2 == @side_3 
+        :equilateral
+       elsif
         (@side_1 = @side_2) || (@side_3 = @side_2) || (@side_1 = @side_3)
         :isosceles
       elsif
-       @side_1 == @side_2 && @side_2 == @side_3 
-        :equilateral
+       (@side_1 != @side_2) && (@side_3 != @side_2) && (@side_1 != @side_3)
+      :scalene
   end
  end 
 end
